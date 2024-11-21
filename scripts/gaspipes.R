85d9a197ade96ca1906b8f9074ae6b91a9b7470b
John Kelly <jkelly3rd@gmail.com>
2024-11-21 10:28:50 -0500

    Update with new map, ignite ind, detroit spreadsheet

--- scripts/gaspipes.R
+++ scripts/gaspipes.R
@@ -32,15 +32,26 @@ gas_incidents_detroit <- leaflet(gasincidents %>%
                    popup = ~paste("<strong>Year:</strong>", iyear, 
                                   "<br><strong>Address:</strong>", location_street_address, 
                                   "<br><strong>City:</strong>", location_city_name, 
-                                  "<br><strong>Fatalities:</strong>", fatal)) %>%
+                                  "<br><strong>Fatalities:</strong>", fatal,
+                                  "<br><strong>Injuries:</strong>", injure,
+                                  "<br><strong>Gas Provider:</strong>", name)) %>%
   addLegend("bottomright", colors = c("red", "blue"), 
             labels = c("Fatal", "Non-fatal"), 
             title = "Incident Type") %>%
-  setView(-83.0458, 42.3314, zoom = 9) %>%
-  addControl(html = "<h3>Gas Incidents in Detroit</h3>", position = "topright")
-
-
+  setView(-83.0458, 42.3314, zoom = 9.5) %>%
+  addControl(html = "<h3>Gas Incidents in Detroit Region<br>From 2010 to October 2024</h3>", position = "topright")
     
 # save an html version of the map into the docs directory
 saveWidget(gas_incidents_detroit, "docs/gas_incidents_detroit.html", selfcontained = TRUE)
 
+# Create a document that gives every incidents in Wayne or Oakland County Michigan with the year, date, street address, city, county, gas company name and the narrative description of the incident and number of fatalities
+detroit_incidents <- gasincidents %>% 
+  filter(location_county_name %in% c("WAYNE", "WAYNE COUNTY","OAKLAND","OAKLAND COUNTY") & location_state_abbreviation == "MI") %>% 
+  select(iyear, local_datetime, location_street_address, location_city_name, location_county_name, name, narrative, fatal,injure)
+# use lubridate to convert the local_datetime to a date
+detroit_incidents <- detroit_incidents %>% 
+  mutate(local_datetime = mdy_hm(local_datetime)) %>% 
+  mutate(date = as_date(local_datetime))
+# write as csv 
+detroit_incidents %>% 
+  write_csv("data/gas_incidents_wayne_oakland.csv")
